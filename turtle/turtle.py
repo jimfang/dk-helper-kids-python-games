942bea04da9529455fa53a7def72a27afaf1ae55
jimfang <jimway21@gmail.com>
2024-05-16 23:08:31 +1000

    add command line

---
+++ turtle/turtle.py
@@ -0,0 +1,21 @@
+
+# Must run from shell window
+# 1. open a shell window 
+# 2. type "python"
+# 3. test follow command
+#     >>> from ....
+
+from turtle import *
+
+# 200 pixel
+forward(200)
+
+# left, right: 90 angles
+left(90)
+
+forward(300)
+
+print("You've unlocked a new level!")
+
+123 + 456 * (7/8)
+''.join(reversed("Time to play"))
